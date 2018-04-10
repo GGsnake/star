f46f38846c5573dff6403db9c27a4dddeb59f4a3
ljp <ljp462693266@outlook.com>
2018-04-10 10:58:56 +0800

    init

--- adc/login.py
+++ adc/login.py
@@ -22,6 +22,21 @@ def login(request):
             return render(request, 'polls/loginerror.html')
     else:
          return render(request, 'polls/loginerror.html')
+def logis(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    if password and username:
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            ip = request.META.get("REMOTE_ADDR", None)
+            logprint.loge("登录", username, "IP:"+ip, "login")
+            return HttpResponseRedirect("index")
+        else:
+            return render(request, 'polls/loginerror.html')
+    else:
+         return render(request, 'polls/loginerror.html')
 
 def loginindex(request):
     return render(request, 'polls/newlogin.html')
